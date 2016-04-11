5bf96174bcc963f1706030d8cd03183b52785e73
deeptipc <deepti.bhardwaj1992@gmail.com>
2016-04-11 17:50:15 +0530

    errors fixedgit status

--- app/controllers/EmployeeController.scala
+++ app/controllers/EmployeeController.scala
@@ -1,4 +1,5 @@
 package controllers
+
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.mvc.{Action, Controller}
@@ -10,6 +11,7 @@ import services.DBManager._
 import scala.concurrent.{Future, Await}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
+
 /**
   * Created by knoldus on 10/4/16.
   */
@@ -42,28 +44,28 @@ class EmployeeController extends Controller{
     Ok(views.html.login(loginForm))
   }
 
-  def register() = Action { implicit request =>
+  def register() = Action.async{ implicit request =>
     addForm.bindFromRequest.fold(
 
-      badForm => BadRequest(views.html.add(badForm)),
+      badForm => Future(Redirect(routes.EmployeeController.show).flashing("error" -> "invalid form!!")),
       validForm => {
-    val name = validForm._1
+        val name = validForm._1
         val password = validForm._2
         val address = validForm._3
         val dob = validForm._4
         val dateOfJoining = validForm._5
         val designation = validForm._6
-        employeeCollection.insert(BSONDocument("name"-> name, "password"->password, "address"->address,"dob"->dob,"joiningDate"->dateOfJoining,"designation"-> designation ) )
+        val re= employeeCollection.insert(BSONDocument("name"-> name, "password"->password, "address"->address,"dob"->dob,"joiningDate"->dateOfJoining,"designation"-> designation ) )
          Thread.sleep(5000)
-          Ok("added")
+         Future(Ok("Employee successfully added!!!"))
       })}
 
-    def validate = Action(implicit request => loginForm.bindFromRequest.fold(
+    def validate = Action{implicit request => loginForm.bindFromRequest.fold(
     badForm => BadRequest(views.html.login(badForm)),
     validForm => {
       val result = Await.result(employeeCollection.find(BSONDocument("name" ->validForm._1 , "password" ->validForm._2)).one[BSONDocument], 10.seconds)
       if(result.isDefined)
-        Ok("match!!"+result+"::")
+        Ok("Employee successfully added!! :)")
       else
-        Redirect(routes.EmployeeController.login).flashing("error" -> "wrong name or paswsword")
-    }))}
+       Redirect(routes.EmployeeController.login).flashing("error" -> "wrong name or paswsword")
+    })}}
